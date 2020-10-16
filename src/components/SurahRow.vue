1c423c66a56ac5f6c637a0dd47b3f3fe57318163
Yousef Kamawall <yousef.kamawall@gmail.com>
2020-10-16 17:33:40 +0200

    refactor: surah rows as separate component

---
+++ src/components/SurahRow.vue
@@ -0,0 +1,56 @@
+<template>
+  <g-link class="row" :to="`/${surah.id}`">
+    <section class="row-inner">
+      <div style="display: flex">
+        <number :num="parseInt(surah.id)" />
+      </div>
+      <article class="row-content">
+        <div style="flex: 1;">
+          <h3>{{ surah.title }}</h3>
+          <p>{{ surah.count }} verses</p>
+        </div>
+        <h2 class="row-title">{{ surah.titleAr }}</h2>
+      </article>
+    </section>
+  </g-link>
+</template>
+
+<script>
+import Number from "./Number";
+
+export default {
+  components: { Number },
+  props: ['surah']
+}
+</script>
+
+<style scoped>
+.row {
+  display: block;
+  width: 100%;
+  color: inherit;
+  padding: 2rem 0;
+  border-bottom: 1px solid var(--border-color);
+}
+.row:hover {
+  background: rgba(255,255,255,0.1);
+}
+.row-inner {
+  display: flex;
+  align-items: center;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.row-content {
+  display: flex;
+  align-items: center;
+  flex: 1;
+}
+.row h2, .row h3 {
+  margin: 0;
+}
+.row p {
+  margin: 0;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,6 @@
 <template>
   <Layout>
-    <g-link v-for="surah in surahs" :key="surah.id" class="row" :to="`/${surah.id}`">
-      <section class="row-inner">
-        <div style="display: flex">
-          <number :num="parseInt(surah.id)" />
-        </div>
-        <article class="row-content">
-          <div style="flex: 1;">
-            <h3>{{ surah.title }}</h3>
-            <p>{{ surah.count }} verses</p>
-          </div>
-          <h2 class="row-title">{{ surah.titleAr }}</h2>
-        </article>
-      </section>
-      <!-- <div class="line"></div> -->
-    </g-link>
+    <surah-row v-for="surah in surahs" :key="surah.id" :surah="surah" />
   </Layout>
 </template>
 
@@ -34,11 +20,12 @@ query {
 </static-query>
 
 <script>
-import Number from "../components/Number";
+import SurahRow from "../components/SurahRow"
 
 export default {
-  components: { Number },
-
+  components: {
+    SurahRow
+  },
   metaInfo() {
     return {
       title: 'Read Quran with Pashto Translation'
@@ -51,41 +38,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.row {
-  display: block;
-  width: 100%;
-  color: inherit;
-  padding: 2rem 0;
-}
-.row:hover {
-  background: rgba(255,255,255,0.1);
-}
-.row-inner {
-  display: flex;
-  align-items: center;
-  max-width: 500px;
-  margin: 0 auto;
-}
-
-.row-content {
-  display: flex;
-  align-items: center;
-  flex: 1;
-}
-.row h2, .row h3 {
-  margin: 0;
-}
-.row p {
-  margin: 0;
-}
-
-.line {
-  width: 100%;
-  height: 1px;
-  display: block;
-  background: var(--border-color);
-  margin: 2.75rem 0 2.75rem 1rem;
-}
-</style>
